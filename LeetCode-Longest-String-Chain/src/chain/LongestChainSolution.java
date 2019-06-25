94a18f5e7361533e91b91daee92b88f963e15009
chaninkou <michaelkoulove@yahoo.com>
2019-06-24 21:11:42 -0700

    Finished another coding problem.

---
+++ LeetCode-Longest-String-Chain/src/chain/LongestChainSolution.java
@@ -0,0 +1,35 @@
+package chain;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class LongestChainSolution {
+    public int longestStrChain(String[] words) {
+        Map<String, Integer> dp = new HashMap<>();
+        
+        Arrays.sort(words, (a,b) -> a.length() - b.length());
+        
+        // For each element in the array
+        for(String word : words){
+            int max = 0;
+            
+            for(int i = 0; i < word.length(); i++){
+                
+                String previous = word.substring(0, i) + word.substring(i + 1);
+                
+                max = Math.max(max, dp.getOrDefault(previous, 0) + 1);
+            }
+            
+            dp.put(word,max);
+        }
+        
+        int res = 0;
+        
+        for(int v : dp.values()){
+            res = Math.max(res, v);
+        }
+        
+        return res;
+    }
+}
---
+++ LeetCode-Longest-String-Chain/src/chain/Main.java
@@ -0,0 +1,16 @@
+package chain;
+
+import java.util.Arrays;
+
+public class Main {
+	public static void main(String[] args){
+		String[] input = {"a","b","ba","bca","bda","bdca"};
+		
+		System.out.println("Input: " + Arrays.toString(input));
+		
+		LongestChainSolution solution = new LongestChainSolution();
+		
+		System.out.println("Solution: " + solution.longestStrChain(input));
+		
+	}
+}
